0d226d8025cc3c59ae97f6a1c26c9e84f25d3541
sjefvanleeuwen <@sjefvanleeuwen>
2020-07-18 12:58:01 +0200

    Adding namespace

---
+++ src/Blazor.AdminLte/NavBar/Dropdown/DropdownFooter.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace Blazor.AdminLte
+{
+    public interface IDropdownFooter
+    {
+        string Link { get; set; }
+    }
+}
---
+++ src/Blazor.AdminLte/NavBar/NavBarBadge.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.AdminLte
+{
+    public class NavBarBadgeEventArgs : EventArgs
+    {
+
+    }
+}
