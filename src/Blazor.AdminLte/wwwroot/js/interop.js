064c882447ec8754e7d47f6cdb5c3f5c8f58a9cf
sjefvanleeuwen <@sjefvanleeuwen>
2020-07-18 13:01:43 +0200

    interops

---
+++ src/Blazor.AdminLte/wwwroot/js/interop.js
@@ -0,0 +1,33 @@
+﻿function toggleMenu(el) {
+    $(el).next().slideToggle('fast');
+}
+
+function hideMenu(el) {
+    $(el).next().hide();
+}
+
+function toggleSideMenu(el) {
+    $(el).slideToggle('fast');
+    
+}
+
+function addClass(el, _class) {
+    $(el).addClass(_class);
+  //  $(el).collapse();
+}
+
+function pushMenu() {
+    $('[data-toggle="push-menu"]').pushMenu('toggle')
+}
+
+function toggle() {
+    $(el).toggle();
+}
+
+function removeClass(el, _class) {
+    $(el).removeClass(_class);
+}
+
+function click(el) {
+    $(el).click();
+}
