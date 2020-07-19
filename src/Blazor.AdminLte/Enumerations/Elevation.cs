664aa58d736f6b1912659636a7f4b366e68f3058
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-07-19 18:26:19 +0200

    Adding image effect and elevation

---
+++ src/Blazor.AdminLte/Enumerations/Elevation.cs
@@ -0,0 +1,18 @@
+﻿namespace Blazor.AdminLte
+{
+    public enum Elevation
+    {
+        [Style("")]
+        None,
+        [Style("elevation-1")]
+        One,
+        [Style("elevation-2")]
+        Two,
+        [Style("elevation-3")]
+        Three,
+        [Style("elevation-4")]
+        Four,
+        [Style("elevation-5")]
+        Five
+    }
+}
---
+++ src/Blazor.AdminLte/Enumerations/ImageEffect.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazor.AdminLte
+{
+    public enum ImageEffect
+    {
+        [Style("")]
+        Square,
+        [Style("img-rounded")]
+        Rounded,
+        [Style("img-circle")]
+        Circle,
+    }
+}
