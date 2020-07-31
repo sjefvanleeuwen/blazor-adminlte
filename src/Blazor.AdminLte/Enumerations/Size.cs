4db1dd89b7042d6872cba176f329184b85f75fc3
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-07-31 13:54:20 +0200

    Size style

---
+++ src/Blazor.AdminLte/Enumerations/Size.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.AdminLte
+{
+    public enum Size
+    {
+        [Style("")]
+        ExtraSmall,
+        [Style("sm")]
+        Small,
+        [Style("md")]
+        Medium,
+        [Style("lg")]
+        Large,
+        [Style("xl")]
+        ExtraLarge
+    }
+}
