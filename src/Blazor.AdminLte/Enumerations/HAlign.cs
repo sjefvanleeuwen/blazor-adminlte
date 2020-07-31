48f4df6f8c8508977913ccff07c90c1c64d3cb02
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-07-31 13:54:11 +0200

    Horizontal Alignment style

---
+++ src/Blazor.AdminLte/Enumerations/HAlign.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.AdminLte
+{
+    public enum HAlign
+    {
+        [Style("center")]
+        Center,
+        [Style("left")]
+        Left,
+        [Style("right")]
+        Right
+    }
+}
