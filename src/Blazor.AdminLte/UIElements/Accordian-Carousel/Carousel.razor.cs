2e3926fd3be25d27fb4a98f47962ab55038cc8f9
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-07-31 18:25:39 +0200

    Implemented Carousel

---
+++ src/Blazor.AdminLte/UIElements/Accordian-Carousel/Carousel.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.AdminLte
+{
+    public class CarouselItem
+    {
+        public string Image { get; set; }
+        public string Alt { get; set; }
+    }
+
+    public class CarouselItemCollection : List<CarouselItem>
+    {
+
+    }
+
+    public partial class Carousel
+    {
+    }
+}
