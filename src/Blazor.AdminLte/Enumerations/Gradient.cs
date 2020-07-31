be6cca069aec5c04a6cec705cc6b77fef2d3ba96
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-07-31 13:53:52 +0200

    Gradient style

---
+++ src/Blazor.AdminLte/Enumerations/Gradient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.AdminLte
+{
+    public enum Gradient
+    {
+        [Style("")]
+        None,
+        [Style("gradient")]
+        Gradient
+    }
+}
