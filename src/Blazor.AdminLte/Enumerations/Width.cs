3845e6c6366229574a30712f2c140bbc782368c3
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-07-31 13:54:30 +0200

    Grid width style

---
+++ src/Blazor.AdminLte/Enumerations/Width.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.AdminLte
+{
+    public enum Width
+    {
+        [Style("")]
+        None,
+        [Style("1")]
+        One,
+        [Style("2")]
+        Two,
+        [Style("3")]
+        Three,
+        [Style("4")]
+        Four,
+        [Style("5")]
+        Five,
+        [Style("6")]
+        Six,
+        [Style("7")]
+        Seven,
+        [Style("8")]
+        Eight,
+        [Style("9")]
+        Nine,
+        [Style("10")]
+        Ten,
+        [Style("11")]
+        Eleven,
+        [Style("12")]
+        Twelve,
+        [Style("auto")]
+        Auto
+    }
+}
