7c96b8fd807b5d33f26bfa6e8648dfc211f25c9c
Marijn Pessers <marijn@pessers.net>
2020-08-07 11:42:23 +0200

    Fixed Gradient Bug

---
+++ src/Blazor.AdminLte/Widgets/Box/InfoBox.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.AdminLte
+{
+    partial class InfoBox
+    {
+        [Parameter]
+        public string Icon { get; set; }
+        [Parameter]
+        public double Number { get; set; }
+        [Parameter]
+        public double? Percentage { get; set; }
+        [Parameter]
+        public Color IconBackgroundColor { get; set; }
+        [Parameter]
+        public Color BackgroundColor { get; set; }
+        [Parameter]
+        public Gradient Gradient { get; set; }
+
+        [Parameter]
+        public RenderFragment ProgressDescription { get; set; }
+        [Parameter]
+        public RenderFragment Title { get; set; }
+
+        private string DisplayBackgroundColor =>
+            BackgroundColor == Color.Default ?
+            string.Empty :
+            $"{GradientPrefix}{BackgroundColor.GetDescription<StyleAttribute>()}";
+
+        private string DisplayIconBackgroundColor => 
+            IconBackgroundColor == Color.Default ?
+            string.Empty :
+            $"{GradientPrefix}{IconBackgroundColor.GetDescription<StyleAttribute>()}";
+
+        private string GradientPrefix =>
+                Gradient == Gradient.None ?
+                string.Empty :
+                $"{Gradient.GetDescription<StyleAttribute>()}-";
+    }
+}
