cb4c9cd8105e2e7e5c399ab63b37b230e03059eb
Johan Bennink <j.bennink@calway.nl>
2020-08-12 14:18:59 +0200

    SmallBox Added

---
+++ src/Blazor.AdminLte/Widgets/Box/SmallBox.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Blazor.AdminLte
+{
+    public partial class SmallBox
+    {
+        [Parameter] 
+        public RenderFragment Title { get; set; } 
+        [Parameter] 
+        public RenderFragment Value { get; set; } 
+        [Parameter] 
+        public string Icon { get; set; } = "";
+        [Parameter] 
+        public Color BackgroundColor { get; set; }
+        [Parameter]
+        public Gradient Gradient { get; set; }
+        [Parameter]
+        public RenderFragment Footer { get; set; }     
+
+        private string DisplayBackgroundColor =>
+           BackgroundColor == Color.Default ?
+           string.Empty :
+           $"{GradientPrefix}{BackgroundColor.GetDescription<StyleAttribute>()}";
+        private string GradientPrefix =>
+               Gradient == Gradient.None ?
+               string.Empty :
+               $"{Gradient.GetDescription<StyleAttribute>()}-";
+    }
+}
