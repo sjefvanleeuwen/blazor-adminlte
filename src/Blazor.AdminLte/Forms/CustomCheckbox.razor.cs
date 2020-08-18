a8f70138ef64cb5db221f116dd92537dcd31a6a7
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-08-18 23:45:24 +0200

    Custom Checkbox

---
+++ src/Blazor.AdminLte/Forms/CustomCheckbox.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.AdminLte
+{
+    public partial class CustomCheckbox
+    {
+        [Parameter]
+        public CustomCheckboxState State { get; set; }
+
+        [Parameter]
+        public EventCallback<CustomCheckboxState> StateChanged { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public EventCallback<ChangeEventArgs> OnChange { get; set; }
+
+        private async void DoChange(ChangeEventArgs e)
+        {
+            if (OnChange.HasDelegate)
+                await OnChange.InvokeAsync(e);
+        }
+    }
+}
---
+++ src/Blazor.AdminLte/Forms/CustomCheckboxState.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.AdminLte
+{
+    public class CustomCheckboxState
+    {
+        public bool? IsChecked { get; set; }
+        public bool IsDisabled { get; set; }
+        public string Value { get; set; }
+        public string Identifier { get; set; }
+        public MarkupString Label { get; set; }
+    }
+}
