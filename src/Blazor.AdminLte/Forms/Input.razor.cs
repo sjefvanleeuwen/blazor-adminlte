30b9837bcdc882bd994f1558d49ba3c55b830562
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-09-03 18:58:31 +0200

    Form input (WIP, unstable)

---
+++ src/Blazor.AdminLte/Forms/Input.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.AdminLte
+{
+    public partial class Input
+    {
+        [Parameter]
+        public InputState Value { get; set; }
+
+        [Parameter]
+        public EventCallback<InputState> ValueChanged { get; set; }
+
+        [Parameter]
+        public EventCallback<InputState> OnChange { get; set; }
+
+        private void DoChange(ChangeEventArgs e)
+        {
+            Value.Value = (string)e.Value;
+            ValueChanged.InvokeAsync(Value);
+            if (OnChange.HasDelegate)
+                OnChange.InvokeAsync(Value);
+        }
+    }
+}
---
+++ src/Blazor.AdminLte/Forms/InputState.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazor.AdminLte
+{
+    public class InputState
+    {
+        public string Value { get; set; }
+        public string Identifier { get; set; }
+        public string Label { get; set; }
+        public string Placeholder { get; set; }
+        public string Type { get; set; }
+        
+    }
+}
