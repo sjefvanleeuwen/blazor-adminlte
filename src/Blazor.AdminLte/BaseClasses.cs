caf79df454bb5fb5a778f35c6d1a853cc235d631
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-10-02 13:43:00 +0200

    Abstraction Component for Classes

---
+++ src/Blazor.AdminLte/BaseClasses.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.AdminLte
+{
+    public class BaseClasses : ComponentBase
+    {
+        [Parameter]
+        public object Classes
+        {
+            get
+            {
+                return classes;
+            }
+            set
+            {
+                classes = value.ToString();
+            }
+        }
+
+        public string classes { get; set; } = "";
+    }
+}
