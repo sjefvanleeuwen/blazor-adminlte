0c5ef34ee391ab27ff4036d5fa88f7052c03e206
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-11-10 12:56:04 +0100

    State object for SideBar

---
+++ src/Blazor.AdminLte/SideBar/SideBarState.cs
@@ -0,0 +1,43 @@
+﻿using BlazorState;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blazor.AdminLte
+{
+    public partial class SideBarState : State<SideBarState>
+    {
+        public string ActiveMenu { get; set; }
+
+        public NavLinkState GetState(string item)
+        {
+            if (item == ActiveMenu)
+                return NavLinkState.Active;
+            else
+                return NavLinkState.Inactive;
+        }
+
+        public override void Initialize()
+        {
+           
+        }
+
+        public class SelectMenuItemAction : IAction
+        {
+            public string Id { get; set; }
+        }
+
+        public class SelectMenuItemHandler : ActionHandler<SelectMenuItemAction>
+        {
+            public SelectMenuItemHandler(IStore aStore) : base(aStore) { }
+
+            SideBarState State => Store.GetState<SideBarState>();
+            public override Task<Unit> Handle(SelectMenuItemAction aAction, CancellationToken aCancellationToken)
+            {
+                State.ActiveMenu = aAction.Id;
+                return Unit.Task;
+            }
+        }
+    }
+}
