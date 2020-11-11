3ce6072c0802b32500db5ec96bb244ed5af5cddf
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-11-11 20:44:15 +0100

    OnClick event for NavBarMenuItem #27

---
+++ src/Blazor.AdminLte/NavBar/NavBarMenuItem.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+
+namespace Blazor.AdminLte
+{
+    public partial class NavBarMenuItem : INavBarMenuItem
+    {
+        [Parameter]
+        public string Link { get; set; } = "";
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public EventCallback<Tuple<INavBarMenuItem, MouseEventArgs>> OnClick { get; set; }
+        private void DoClick(MouseEventArgs args)
+        {
+            OnClick.InvokeAsync(new Tuple<INavBarMenuItem, MouseEventArgs>(this, args));
+        }
+    }
+
+    public interface INavBarMenuItem
+    {
+        string Link { get; set; }
+    }
+}
