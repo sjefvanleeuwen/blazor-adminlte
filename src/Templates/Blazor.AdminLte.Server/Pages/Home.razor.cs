77b3c3edd3671033e7cbb5689d491b241d116c13
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-01-17 22:06:21 +0100

    Custom menu examples

--- src/Templates/Blazor.AdminLte.Server/Pages/Home.razor.cs
+++ src/Templates/Blazor.AdminLte.Server/Pages/Home.razor.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
 
 namespace Blazor.AdminLte.Server.Pages
 {
     public partial class Home
     {
-        [Inject]
-        NavBarLeftInjectableMenu menu { get; set; }
+
     }
+
 }
