308cc7e42ad93cdbf79f27387215e8ab0b6978c6
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2022-09-10 13:07:07 +0200

    audio player

---
+++ src/Blazor.AdminLte/Audio/PlayerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Blazor.AdminLte
+{
+    public class PlayerService : IPlayerService
+    {
+        public Player Player { get; set; }
+
+
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void Play(string url)
+        {
+            await Player.Submit();
+        }
+
+        public void Resume()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IPlayerService
+    {
+        Player Player { get; set; }
+        void Pause();
+        void Resume();
+        void Play(string url);
+    }
+}
